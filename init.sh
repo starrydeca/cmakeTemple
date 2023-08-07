49d595376c153ad58130c5a60800a5961e26b580
StarsDecade <3519506439@qq.com>
2023-08-07 17:48:14 +0800

    优化使用效果

--- init.sh
+++ init.sh
@@ -8,6 +8,13 @@ mkdir build #存放cmake
 echo -n "输入项目名称："
 read name 
 
+#修改README.md的文件中项目名称
 echo "# ${name}" > README.md
 
-rm -rf init.sh 
+#修改CMakeLists.txt中项目名称
+sed "s/name/${name}/g" CMakeLists.txt
+
+#把文件转移到项目路径中
+mv !(init.sh) ..
+
+rm -rf ../cmakeTemple  
